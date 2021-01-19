874e330c0d160b80817113a5da9d2a990f58dee8
gforsythe <gabe.forsythe@gmail.com>
2021-01-19 17:19:39 +0000

    added the long url link that works and nodemon

--- express_server.js
+++ express_server.js
@@ -3,14 +3,14 @@ const app = express()
 const PORT = 8080;
 const bodyParser = require('body-parser');
 const urlDatabase = {
-  'b2xVn2': 'htttp:www.lighthouselabs.ca',
+  'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xk': 'http://www.google.com'
 };
 
 function generateRandomString(){
-  var result           = '';
-  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  var charactersLength = characters.length;
+  let result = '';
+  let characters= 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
   for ( var i = 0; i < 6; i++ ) {
    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
@@ -21,17 +21,18 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/',(req, res) => {
-  res.send('Hello');
-});
+// app.get('/',(req, res) => {
+  // res.send('Hello');
+// });
 
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
 app.post('/urls', (req, res) => {
-  console.log(req.body);
-  res.send('Ok');
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req["body"]["longURL"];
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get('/urls', (req, res) => {
@@ -49,9 +50,14 @@ app.get('/urls', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n')
+app.get('/u/:shortURL', (req, res) => {
+const longURL = urlDatabase[req.params.shortURL];
+res.redirect(longURL);
 })
+
+// app.get('/hello', (req, res) => {
+//   res.send('<html><body>Hello <b>World</b></body></html>\n')
+// })
 
 app.listen(PORT, () => {
   console.log(`Example app listeniing on port ${PORT}!`);
