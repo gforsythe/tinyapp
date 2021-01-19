da4eef094672cafda9bb4ca0e2d3632120328394
gforsythe <gabe.forsythe@gmail.com>
2021-01-19 03:17:02 +0000

    added post, checked out network panel and ID generator.

--- express_server.js
+++ express_server.js
@@ -1,21 +1,44 @@
 const express = require('express');
 const app = express()
 const PORT = 8080;
+const bodyParser = require('body-parser');
 const urlDatabase = {
   'b2xVn2': 'htttp:www.lighthouselabs.ca',
   '9sm5xk': 'http://www.google.com'
 };
 
+function generateRandomString(){
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < 6; i++ ) {
+   result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/',(req, res) => {
   res.send('Hello');
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
 });
 
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
+
 
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL] }
